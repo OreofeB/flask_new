7387f4d494c25e4bf8f43e1ad9d8bc721c5a0672
OreofeB <115746859+OreofeB@users.noreply.github.com>
2024-02-19 22:27:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,8 @@ def preprocess_data(data_df):
     # new_data = new_data.dropna()
     # new_data = new_data[new_data['status_id'] != 1]
 
-    remaining_fields = ['gender', 'marital_status', 'no_of_dependent', 'type_of_residence', 'educational_attainment',
-                    'employment_status', 'sector_of_employment', 'monthly_net_income', 'country', 'city', 'lga',
-                    'requested_amount', 'purpose', 'proposed_payday', 'loan_request_hour', 'age', 'selfie_bvn_check',
-                    'selfie_id_check', 'phone_network', 'loan_requests', 'failed_loan_requests', 'successful_loan_requests',
-                    'loans', 'lending_lenders', 'lenders', 'emails', 'phone_numbers', 'device_name', 'mobile_os',
-                    'os_version', 'bank']
+    remaining_fields = ['no_of_dependent', 'loan_request_hour', 'age', 'phone_network', 'loan_requests', 'failed_loan_requests',
+                        'successful_loan_requests', 'loans', 'lending_lenders', 'lenders', 'emails', 'phone_numbers', 'bank']
     
     new_data[remaining_fields] = new_data[remaining_fields].astype(int)
 
